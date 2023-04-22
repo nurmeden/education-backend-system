065b3819429750bfaa8bd4199a4b43f2f9b4d13f
nurmeden <nurmeden>
2023-04-22 17:12:02 +0500

    upd

--- cmd/main.go
+++ cmd/main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	cfg, err := config.LoadConfig("configs/config.yaml")
+	cfg, err := config.LoadConfig("configs/configs.yaml")
 	if err != nil {
 		log.Fatalf("Failed to load configuration: %v", err)
 	}
