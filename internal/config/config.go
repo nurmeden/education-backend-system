1288b46c20a108a929644c75f5578793150b3f89
nurmeden <nurmeden>
2023-04-22 10:53:22 +0500

    changing LoadConfig because of configs

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,37 +1,24 @@
 package config
 
 import (
-	"fmt"
-
 	"github.com/spf13/viper"
 )
 
 type Config struct {
-	Port        string
-	DatabaseUrl string
+	Port        string `mapstructure:"port"`
+	DatabaseUrl string `mapstructure:"database_url"`
 }
 
 func LoadConfig(configPath string) (*Config, error) {
-	v := viper.New()
-	v.SetConfigFile(configPath)
-
-	// Указываем, что используем переменные окружения для настройки
-	v.SetEnvPrefix("MY_MICROSERVICE")
-	v.AutomaticEnv()
-
-	if err := v.ReadInConfig(); err != nil {
-		return nil, fmt.Errorf("failed to read config file: %v", err)
+	viper.SetConfigFile(configPath)
+	if err := viper.ReadInConfig(); err != nil {
+		return nil, err
 	}
 
-	// Указываем значения по умолчанию, если они не указаны
-	v.SetDefault("port", "8080")
-	v.SetDefault("database_url", "postgres://user:password@localhost/dbname")
-
-	// Создаем экземпляр структуры Config и привязываем значения конфигурации
-	cfg := &Config{}
-	if err := v.Unmarshal(cfg); err != nil {
-		return nil, fmt.Errorf("failed to unmarshal config: %v", err)
+	var config Config
+	if err := viper.Unmarshal(&config); err != nil {
+		return nil, err
 	}
 
-	return cfg, nil
+	return &config, nil
 }
