fd6fdfc3741c54b51d4b484efda8ca0a17ac524f
nurmeden <nurmeden>
2023-04-22 15:51:53 +0500

    initializing the database

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"task-backend/internal/config"
+	"task-backend/internal/data/database"
 )
 
 func main() {
@@ -10,5 +11,11 @@ func main() {
 	if err != nil {
 		log.Fatalf("Failed to load configuration: %v", err)
 	}
+
+	db, err := database.NewDatabase(cfg.DatabaseUrl, cfg.DatabaseName, cfg.CollectionName)
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+	defer db.Close()
 
 }
