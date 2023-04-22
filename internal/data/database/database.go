7d7fc78f225edb8ac2db09466f95024c9d3c5689
nurmeden <nurmeden>
2023-04-22 11:13:59 +0500

    configuring connection options

--- internal/data/database/database.go
+++ internal/data/database/database.go
@@ -2,8 +2,10 @@ package database
 
 import (
 	"fmt"
+	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type MongoDbConfig struct {
@@ -22,11 +24,13 @@ type MongoDbClient struct {
 }
 
 func NewMongoDbClient(config MongoDbConfig) (*MongoDbClient, error) {
-	connectionUrl = fmt.Sprintf("mongodb://%s:%s@%s:%d",
+	connectionURI := fmt.Sprintf("mongodb://%s:%s@%s:%d",
 		config.Username,
 		config.Password,
 		config.Host,
 		config.Port)
 
+	clientOptions := options.Client().ApplyURI(connectionURI)
+	clientOptions.SetConnectTimeout(10 * time.Second)
 	return config, nil
 }
